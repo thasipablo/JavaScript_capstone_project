21894e188382f2cf23309d3d71ede1fa5f7de5ba
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-08-07 16:52:10 -0600

    Setup webpack and initial files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)|(css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leaderboard',
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
+};
