0cc030428899170e6cd582f48d36db942b9768aa
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-08-09 21:28:58 +0100

    add click event on book cards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import fetchBooks from './modules/api.js';
-import displayBooks from './modules/books.js';
-import './style.css';
-import Logo from '../assets/logoW.svg';
-import 'font-awesome/css/font-awesome.min.css';
+import { fetchBooks, getBook, books } from "./modules/api.js";
+import displayBooks from "./modules/books.js";
+import "./style.css";
+import Logo from "../assets/logoW.svg";
+import "font-awesome/css/font-awesome.min.css";
 
-const logo = document.getElementById('logo');
+const logo = document.getElementById("logo");
 logo.src = Logo;
 
-fetchBooks().then((data) => {
-  displayBooks(data);
+displayBooks(books);
+
+const commentBtnTags = document.querySelectorAll(".comment-btn");
+commentBtnTags.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const book = getBook(btn.id);
+    console.log(btn.id, book);
+  });
 });
