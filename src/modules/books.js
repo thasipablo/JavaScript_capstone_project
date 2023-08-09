9f51f5c7bbde8608b6c977d768fe9cf32029ddc9
Cristian Villa <c.villavirgen@gmail.com>
2023-08-08 19:11:08 -0600

    Improve overall style in cards

--- src/modules/books.js
+++ src/modules/books.js
@@ -8,8 +8,10 @@ const displayBooks = (data) => {
         <img src="${book.formats['image/jpeg']}" alt="${book.title}">
         <h2>${book.title}</h2>
         <p>${book.authors[0].name}</p>
+        <div class="button-container">
         <button class="like-btn"><i class="fa fa-thumbs-up"></i></button>
         <button class="comment-btn"><i class="fa fa-comment"></i></button>
+        </div>
     </div>
 `;
     container.innerHTML += bookCard;
