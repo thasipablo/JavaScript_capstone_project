8e2955501f73580648eab96c694c8e390aec1ce7
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-08-09 19:18:56 +0100

    display the font awesome icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import fetchBooks from './modules/api.js';
-import displayBooks from './modules/books.js';
-import './style.css';
-import Logo from '../assets/logoW.svg';
-import 'font-awesome/css/font-awesome.min.css';
+import fetchBooks from "./modules/api.js";
+import displayBooks from "./modules/books.js";
+import "./style.css";
+import Logo from "../assets/logoW.svg";
+import "font-awesome/css/font-awesome.min.css";
 
-const logo = document.getElementById('logo');
+const logo = document.getElementById("logo");
 logo.src = Logo;
 
 fetchBooks().then((data) => {
