de4e68f172ffd37e3e5af0af652aaa35316dc906
Cristian Villa <c.villavirgen@gmail.com>
2023-08-10 16:40:14 -0600

    Fix eslint issues

--- src/modules/books.js
+++ src/modules/books.js
@@ -1,15 +1,13 @@
-import displayCommentPopup from "./commentPopup.js";
-import fetchLikes from "./likes.js";
-import displayReservations from "./reservationsPopup.js";
+import displayCommentPopup from './commentPopup.js';
+import fetchLikes from './likes.js';
+import displayReservations from './reservationsPopup.js';
 
-const LIKES_API_URL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/I11ph9MsLdYF8zFhvLLF/likes/";
-const RESERVATION_API_URL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/I11ph9MsLdYF8zFhvLLF/reservations";
+const LIKES_API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/I11ph9MsLdYF8zFhvLLF/likes/';
+const RESERVATION_API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/I11ph9MsLdYF8zFhvLLF/reservations';
 
 const displayBooks = async (data) => {
-  const container = document.createElement("div");
-  container.id = "books-container";
+  const container = document.createElement('div');
+  container.id = 'books-container';
 
   const likesData = await fetchLikes();
 
@@ -17,10 +15,10 @@ const displayBooks = async (data) => {
     const bookLikes = likesData.find((like) => like.item_id === book.id) || {
       likes: 0,
     };
-    const bookCard = document.createElement("div");
-    bookCard.className = "book-card";
+    const bookCard = document.createElement('div');
+    bookCard.className = 'book-card';
     bookCard.innerHTML = `
-      <img src="${book.formats["image/jpeg"]}" alt="${book.title}">
+      <img src="${book.formats['image/jpeg']}" alt="${book.title}">
       <h2>${book.title}</h2>
       <p class="author">${book.authors[0].name}</p>
       <div class="button-container">
@@ -34,23 +32,23 @@ const displayBooks = async (data) => {
       `;
     container.appendChild(bookCard);
 
-    bookCard.querySelector(".reservation-btn").addEventListener("click", () => {
+    bookCard.querySelector('.reservation-btn').addEventListener('click', () => {
       document.body.appendChild(displayReservations(RESERVATION_API_URL, book));
     });
     //
-    bookCard.querySelector(".comment-btn").addEventListener("click", () => {
+    bookCard.querySelector('.comment-btn').addEventListener('click', () => {
       displayCommentPopup(RESERVATION_API_URL, book);
     });
   });
 
-  document.body.insertBefore(container, document.querySelector("footer"));
-  document.querySelectorAll(".like-btn").forEach((btn, index) => {
-    btn.addEventListener("click", async () => {
+  document.body.insertBefore(container, document.querySelector('footer'));
+  document.querySelectorAll('.like-btn').forEach((btn, index) => {
+    btn.addEventListener('click', async () => {
       const book = data[index];
       const response = await fetch(LIKES_API_URL, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           item_id: book.id,
@@ -58,7 +56,7 @@ const displayBooks = async (data) => {
       });
 
       if (response.ok) {
-        const likeCountElement = btn.querySelector(".likes-counter");
+        const likeCountElement = btn.querySelector('.likes-counter');
         const currentLikes = parseInt(likeCountElement.textContent, 10);
         likeCountElement.textContent = `${currentLikes + 1}`;
       }
--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -1,10 +1,10 @@
 const displayCommentPopup = (apiURL, bookData) => {
-  const curtain = document.createElement("div");
-  curtain.id = "curtain";
-  const reservationCtr = document.createElement("div");
-  reservationCtr.classList.add("popup-content");
+  const curtain = document.createElement('div');
+  curtain.id = 'curtain';
+  const reservationCtr = document.createElement('div');
+  reservationCtr.classList.add('popup-content');
   reservationCtr.innerHTML = `
-        <img src="${bookData.formats["image/jpeg"]}" alt="${bookData.title}">
+        <img src="${bookData.formats['image/jpeg']}" alt="${bookData.title}">
         <h2>${bookData.title}</h2>
         <p class="author">${bookData.authors[0].name}</p>
         <div class="comments-container">
@@ -22,17 +22,17 @@ const displayCommentPopup = (apiURL, bookData) => {
         </form>
     `;
   curtain.appendChild(reservationCtr);
-  const closeBtn = document.createElement("button");
-  closeBtn.setAttribute("type", "button");
-  closeBtn.id = "close-btn";
+  const closeBtn = document.createElement('button');
+  closeBtn.setAttribute('type', 'button');
+  closeBtn.id = 'close-btn';
   closeBtn.innerHTML = '<i class="fa-regular fa-circle-xmark"></i>';
   curtain.appendChild(closeBtn);
   document.body.appendChild(curtain);
-  document.querySelector("html").style.overflowY = "hidden";
+  document.querySelector('html').style.overflowY = 'hidden';
 
-  closeBtn.addEventListener("click", () => {
+  closeBtn.addEventListener('click', () => {
     curtain.remove();
-    document.querySelector("html").style.overflowY = "auto";
+    document.querySelector('html').style.overflowY = 'auto';
   });
 };
 
