e56ceef7a371fd05f7a5ebc4399c444b46b5ddd1
thasipablo <thasipablo@gmail.com>
2023-08-11 06:58:38 -0400

    fix lint errors

--- src/modules/commentPopup.test.js
+++ src/modules/commentPopup.test.js
@@ -1,33 +1,31 @@
-import { populateComments } from "./commentPopup.js";
+import { populateComments } from './commentPopup.js';
 
-test("Comment counter", () => {
+test('Comment counter', () => {
   const comments = [
     {
-      comment: "Good book",
-      creation_date: "2023-08-10",
-      username: "Paolo",
+      comment: 'Good book',
+      creation_date: '2023-08-10',
+      username: 'Paolo',
     },
     {
-      comment: "comment",
-      creation_date: "2023-08-10",
-      username: "Ga",
+      comment: 'comment',
+      creation_date: '2023-08-10',
+      username: 'Ga',
     },
     {
-      comment: "Like it",
-      creation_date: "2023-08-10",
-      username: "Jo",
+      comment: 'Like it',
+      creation_date: '2023-08-10',
+      username: 'Jo',
     },
   ];
-  const commentCtr = document.createElement("div");
-  const commentCounter = document.createElement("h3");
-  commentCounter.id = "comments-count";
-  const commentsList = document.createElement("ul");
+  const commentCtr = document.createElement('div');
+  const commentCounter = document.createElement('h3');
+  commentCounter.id = 'comments-count';
+  const commentsList = document.createElement('ul');
   commentCtr.append(commentCounter, commentsList);
   populateComments(comments, commentsList);
 
-  
-
-  const reservationCount = commentsList.querySelectorAll("li").length;
+  const reservationCount = commentsList.querySelectorAll('li').length;
 
   expect(reservationCount).toBe(3);
 });
